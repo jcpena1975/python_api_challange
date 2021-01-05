b2891120eeb036ddf07b0e386abea49b9dccc7ad
jcpena1975 <68211069+jcpena1975@users.noreply.github.com>
2021-01-04 22:05:08 -0600

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+weather_api_key = "8ee320d00a76af787d879f8bc76620aa"
+
+# Google API Key
+<<<<<<< HEAD
+g_key = "AIzaSyBvuF3skZHk7_XD6jDxv9RbrE9xdKtRhDU"
+=======
+g_key = "YOUR KEY HERE!"
+>>>>>>> 296985d19b0d06d89b953cb60301d6548fa2c3e8
